d9229c65937447400b3c92cafcc301c25a585175
ed <eduard.chduaikin@gmail.com>
2017-12-05 09:20:33 +0200

    working sigle file google speech

--- speech-recognition/google-speech-request.py
+++ speech-recognition/google-speech-request.py
@@ -1,14 +1,40 @@
-from google.cloud import storage
+import io
+import os
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+# export GOOGLE_APPLICATION_CREDENTIALS="/home/ed/Work/usefull_code/speech-recognition/key/chu-dev-5f449a6c1a98.json"
+# ffmpeg -i stereo.flac -ac 1 mono.flac
 
 def implicit():
     print('----->Works')
-    # If you don't specify credentials when constructing the client, the
-    # client library will look for credentials in the environment.
-    storage_client = storage.Client()
+    # Instantiates a client
+    client = speech.SpeechClient()
 
-    # Make an authenticated API request
-    buckets = list(storage_client.list_buckets())
-    print(buckets)
+    # The name of the audio file to transcribe
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'audio',
+        'mono.flac')
+    print('----->%s',file_name )
+
+    # Loads the audio into memory
+    with io.open(file_name, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = types.RecognitionAudio(content=content)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        language_code='en-US')
+
+    # Detects speech in the audio file
+    response = client.recognize(config, audio)
+
+    for result in response.results:
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+    # [END speech_quickstart]
+        with open("text/mono.txt", "w") as text_file:
+            print(result.alternatives[0].transcript, file=text_file)
 
 if __name__ == '__main__':
     implicit()
